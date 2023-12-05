8d01a89fa9336cd072408c6c1fd6f692add77cb6
forms5nv50 <152238731+forms5nv50@users.noreply.github.com>
2023-12-04 21:24:50 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,5 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
   apiKey: "AIzaSyDOpR-Ept16bX781KnmfIP2v9MA1PnvyGo",
   authDomain: "proyecto-ahm.firebaseapp.com",
   projectId: "proyecto-ahm",
@@ -7,4 +9,4 @@ const firebaseConfig = {
   messagingSenderId: "620283301335",
   appId: "1:620283301335:web:e92096c088ae08c71c0f92",
   measurementId: "G-B60DT98CT3"
-};
+    });
