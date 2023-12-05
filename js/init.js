2eefb3ec1cfb0a64a92c5a278654ad10ba568d1c
forms5nv50 <152238731+forms5nv50@users.noreply.github.com>
2023-12-04 21:15:50 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -10,13 +10,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA69iFjcZ_n-ARm0WeRrzYbELW_Bq9aHhc",
-  authDomain: "aut50ahm.firebaseapp.com",
-  projectId: "aut50ahm",
-  storageBucket: "aut50ahm.appspot.com",
-  messagingSenderId: "579674238051",
-  appId: "1:579674238051:web:1282a2c8488d4aa3e6dc56",
-  measurementId: "G-P3DGG59F6L"
+  apiKey: "AIzaSyDOpR-Ept16bX781KnmfIP2v9MA1PnvyGo",
+  authDomain: "proyecto-ahm.firebaseapp.com",
+  projectId: "proyecto-ahm",
+  storageBucket: "proyecto-ahm.appspot.com",
+  messagingSenderId: "620283301335",
+  appId: "1:620283301335:web:e92096c088ae08c71c0f92",
+  measurementId: "G-B60DT98CT3"
 };
 
 // Initialize Firebase
